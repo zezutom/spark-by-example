8156cbd6efe0a977de08839ebfdf421a51492c7b
tomaszezula <tomas.zezula@panaseer.com>
2015-12-28 14:55:59 +0100

    Popular hashtags: unit tests

--- src/test/scala/streaming/PopularTweetsTest.scala
+++ src/test/scala/streaming/PopularTweetsTest.scala
@@ -9,7 +9,7 @@ import twitter4j.json.DataObjectFactory
 
 class PopularTweetsTest extends StreamingSuiteBase {
 
-  test("single hashtag transformation") {
+  test("sanity check") {
     verify(List(List(status("#a"))), List(List(("#a", 1))))
   }
 
@@ -18,11 +18,20 @@ class PopularTweetsTest extends StreamingSuiteBase {
   }
 
   test("two hashtags: same count, lexical order applies") {
-    verify(List(List(status("#a"), status("#b"))), List(List(("#a", 1), ("#b", 1))))
+    verify(List(List(status("#b"), status("#a"))), List(List(("#a", 1), ("#b", 1))))
+  }
+
+  test("same hashtag multiple times") {
+    verify(List(List(status("#a"), status("#a"), status("#a"))), List(List(("#a", 3))))
+  }
+
+  test("multiple hashtags, top counts first, lexical order next") {
+    verify(List(List(status("#a"), status("#b #a"), status("#a"), status("#d"), status("#b #c"), status("#c"))),
+      List(List(("#a", 3), ("#b", 2), ("#c", 2), ("#d", 1))))
   }
 
   private def verify(input: List[List[Status]], expected: List[List[(String, Int)]]): Unit = {
-    testOperation[Status, (String, Int)](input, popularTweets().transform _, expected, useSet = true)
+    testOperation[Status, (String, Int)](input, popularTweets().transform, expected, useSet = true)
   }
 
   private def popularTweets(): PopularTweets = new PopularTweets(Seconds(60 * 2), Seconds(1))
