4e96eca1e04fa7fad1735fa5c90b473cd4fef2f0
tomaszezula <tomas.zezula@panaseer.com>
2015-12-29 18:13:34 +0100

    Popular tweets count via a built-in transformation.

--- src/main/scala/streaming/PopularTweets.scala
+++ src/main/scala/streaming/PopularTweets.scala
@@ -13,19 +13,18 @@ import twitter4j.Status
   *
   * $SPARK_HOME/bin/spark-submit \
   * --class "streaming.PopularTweets" \
-  * target/scala-2.11/spark-by-example-assembly-1.0.jar
+  * target/scala-2.10/spark-by-example-assembly-1.0.jar
   */
-class PopularTweets(val windowDuration: Duration, val slideDuration: Duration) extends StreamTransformer[Status, (String, Int)] {
-  override def transform(stream: DStream[Status]): DStream[(String, Int)] =
+class PopularTweets(val windowDuration: Duration, val slideDuration: Duration) extends StreamTransformer[Status, (String, Long)] {
+  override def transform(stream: DStream[Status]): DStream[(String, Long)] =
     stream
       .map(_.getText)
       .flatMap(_.split(" "))
       .filter(_.startsWith("#"))
       .filter(_.matches("^#[a-zA-Z0-9]+$"))
-      .map((_, 1))
-      .reduceByKeyAndWindow(_ + _, _ - _, windowDuration, slideDuration)
+      .countByValueAndWindow(windowDuration, slideDuration)
+      .transform(_.sortByKey())
       .transform(_.sortBy(_._2, ascending = false))
-      .transform(_.sortBy(_._1))
 }
 
 object PopularTweets {
@@ -33,13 +32,13 @@ object PopularTweets {
   def main(args: Array[String]) {
 
     // Instantiate streaming context
-    val ssc = AppUtil.ssc("Trending Tweets", 1)
+    val ssc = AppUtil.ssc("Popular Tweets", 1)
 
     // Connect to Twitter's public stream
     val stream = TwitterUtils.createStream(ssc, AppUtil.twitterAuth())
 
-    // Watch for popular tweets (2-min sliding window)
-    new PopularTweets(Seconds(60 * 2), Seconds(1))
+    // Watch for popular tweets over the last five minutes
+    new PopularTweets(Seconds(60 * 5), Seconds(1))
       .transform(stream)
       .print(10)
 
--- src/test/scala/streaming/PopularTweetsTest.scala
+++ src/test/scala/streaming/PopularTweetsTest.scala
@@ -10,31 +10,31 @@ import twitter4j.json.DataObjectFactory
 class PopularTweetsTest extends StreamingSuiteBase {
 
   test("sanity check") {
-    verify(List(List(status("#a"))), List(List(("#a", 1))))
+    verify(List(List(status("#a"))), List(List(("#a", 1L))))
   }
 
   test("single tweet, two hashtags: same count, lexical order applies") {
-    verify(List(List(status("#a #b"))), List(List(("#a", 1), ("#b", 1))))
+    verify(List(List(status("#a #b"))), List(List(("#a", 1L), ("#b", 1L))))
   }
 
   test("two hashtags: same count, lexical order applies") {
-    verify(List(List(status("#b"), status("#a"))), List(List(("#a", 1), ("#b", 1))))
+    verify(List(List(status("#b"), status("#a"))), List(List(("#a", 1L), ("#b", 1L))))
   }
 
   test("same hashtag multiple times") {
-    verify(List(List(status("#a"), status("#a"), status("#a"))), List(List(("#a", 3))))
+    verify(List(List(status("#a"), status("#a"), status("#a"))), List(List(("#a", 3L))))
   }
 
   test("multiple hashtags, top counts first, lexical order next") {
     verify(List(List(status("#a"), status("#b #a"), status("#a"), status("#d"), status("#b #c"), status("#c"))),
-      List(List(("#a", 3), ("#b", 2), ("#c", 2), ("#d", 1))))
+      List(List(("#a", 3L), ("#b", 2L), ("#c", 2L), ("#d", 1L))))
   }
 
-  private def verify(input: List[List[Status]], expected: List[List[(String, Int)]]): Unit = {
-    testOperation[Status, (String, Int)](input, popularTweets().transform _, expected, useSet = true)
+  private def verify(input: List[List[Status]], expected: List[List[(String, Long)]]): Unit = {
+    testOperation[Status, (String, Long)](input, popularTweets().transform _, expected, useSet = true)
   }
 
-  private def popularTweets(): PopularTweets = new PopularTweets(Seconds(60 * 2), Seconds(1))
+  private def popularTweets() = new PopularTweets(Seconds(60 * 2), Seconds(1))
 
   private def status(text: String): Status = {
     val json = new JSONObject().put("text", text)
